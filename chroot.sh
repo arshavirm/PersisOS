4b8210abe1719a9a6479d957d914eda209513d61
arshavir mirzakhani <76659587+arshavirm@users.noreply.github.com>
2026-07-19 10:10:24 +0330

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -60,5 +60,7 @@ apt clean
 
 rm -rf /var/lib/apt/lists/*
 
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$UID/bus
+
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/persisos/background_1.png
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/last-image --set /usr/share/backgrounds/persisos/background_1.png
