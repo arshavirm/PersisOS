15936582950fc7256d450c7137fe66f9611ca44b
arshavirmirzakhani <2468arshavir>
2026-07-22 19:40:57 +0330

    fix

--- chroot.sh
+++ chroot.sh
@@ -63,8 +63,6 @@ sudo mkdir -p /etc/calamares/
 sudo cp -r /persisos_temp/calamares/persisos /etc/calamares/
 sudo cp -r /persisos_temp/calamares/settings.conf /etc/calamares/settings.conf
 
-sudo rm /root/Desktop/calamares-install-debian.desktop
-
 sudo cp /persisos_temp/calamares-install-persisos.desktop /root/Desktop/calamares-install-persisos.desktop
 sudo cp /persisos_temp/calamares-install-persisos.desktop /etc/skel/Desktop/calamares-install-persisos.desktop
 
