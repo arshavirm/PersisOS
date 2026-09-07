4867c0f01b9f8fe6a4735f6fd32f6bf90425b452
arshavirmirzakhani <2468arshavir>
2026-09-07 23:54:20 +0330

    update

--- assets/persisos-plasma-theme/usr/share/plasma/layout-templates/org.persisos.desktop.defaultlayout/contents/layout.js
+++ assets/persisos-plasma-theme/usr/share/plasma/layout-templates/org.persisos.desktop.defaultlayout/contents/layout.js
@@ -1,37 +1,32 @@
 // PersisOS default desktop layout
 
-var allDesktops = desktops()
+var wallpaper = "file:///usr/share/wallpapers/PersisOS-1/contents/images/2560x1600.png"
+var allDesktops = desktopsForActivity(currentActivity())
 for (var i = 0; i < allDesktops.length; i++) {
-  var d = allDesktops[i]
-  d.wallpaperPlugin = 'org.kde.image'
-  d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General']
-  d.writeConfig('Image', 'file:///usr/share/wallpapers/PersisOS-1/contents/images/2560x1600.png')
-  d.writeConfig('FillMode', 2)
+    var desktop = allDesktops[i]
+    desktop.wallpaperPlugin = "org.kde.image"
+    desktop.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
+    desktop.writeConfig("Image", wallpaper)
+    desktop.writeConfig("FillMode", 2)
 }
 
 var existingPanels = panels()
-for (var p = 0; p < existingPanels.length; p++) {
-  existingPanels[p].remove()
+for (var p = existingPanels.length - 1; p >= 0; p--) {
+    existingPanels[p].remove()
 }
 
 var panel = new Panel
-panel.location = 'top'
-panel.height = gridUnit * 2.2
-panel.floating = false
-panel.alignment = 'left'
-panel.lengthMode = 'fill'
+panel.location = "top"
+panel.height = 2 * Math.floor(gridUnit * 2.5 / 2)
 
-var appmenu = panel.addWidget('org.kde.plasma.kickoff')
-appmenu.currentConfigGroup = ['General']
-appmenu.writeConfig('icon', '/usr/share/icons/hicolor/scalable/apps/persisos-launcher.svg')
-appmenu.currentConfigGroup = ['Shortcuts']
-appmenu.writeConfig('global', 'Meta')
+var appmenu = panel.addWidget("org.kde.plasma.kickoff")
+appmenu.currentConfigGroup = ["General"]
+appmenu.writeConfig("icon", "persisos-launcher")
+appmenu.currentConfigGroup = ["Shortcuts"]
+appmenu.writeConfig("global", "Meta")
 
-panel.addWidget('org.kde.plasma.icontasks')
-panel.addWidget('org.kde.plasma.panelspacer')
-panel.addWidget('org.kde.plasma.systemtray')
-panel.addWidget('org.kde.plasma.digitalclock')
-panel.addWidget('org.kde.plasma.showdesktop')
-
-panel.currentConfigGroup = ['General']
-panel.writeConfig('floating', false)
+panel.addWidget("org.kde.plasma.icontasks")
+panel.addWidget("org.kde.plasma.panelspacer")
+panel.addWidget("org.kde.plasma.systemtray")
+panel.addWidget("org.kde.plasma.digitalclock")
+panel.addWidget("org.kde.plasma.showdesktop")
